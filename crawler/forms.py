b92ed785c71a427a15f6d22b265bceb660dca18c
Daniil Digtyar <daniildigtyar@gmail.com>
2017-11-18 17:19:04 +0100

    Style changed

--- crawler/forms.py
+++ crawler/forms.py
@@ -4,8 +4,18 @@ class SearchUser(forms.Form):
     name1 = forms.CharField(
         label='Username1',
         error_messages={'required': 'Introdueix un usuari'},
+        widget=forms.TextInput(
+        	attrs={
+        		'class':'form-control form-control-lg',
+        		'placeholder':'Nicolas Tesla...'}
+        )
     )
     name2 = forms.CharField(
         label='Username2',
         error_messages={'required': 'Introdueix un usuari'},
+         widget=forms.TextInput(
+        	attrs={
+        		'class':'form-control form-control-lg',
+        		'placeholder':'Thomas Edison...'}
+        )
     )
