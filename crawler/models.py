55aa205fa1f38ec443302676a2f080b5d27f0673
TiberiuTasnadi <ttasnadi.olivera@gmail.com>
2017-11-18 18:53:03 +0100

    database model

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=200, primary_key=True)
+    followers=models.IntegerField()
+    follows=models.IntegerField()
+    facebook_vinculado=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Photos(models.Model):
+    idphoto=model.IntegerField(primary_key=True)
+    username_vinculed=model.ForeignKey(User,on_delete=models.CASCADE)
+    likes=models.IntegerField()
+    coments=models.IntegerField()
+    hastags=TextField()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
--- crawler/views.py
+++ crawler/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from .forms import SearchUser
 from django.http import HttpResponse, HttpResponseRedirect
+
 def inici(request):
     voidSearchUser = SearchUser()
     if request.method == "POST":
         name1=request.POST.get('name1', '')
         name2=request.POST.get('name2', '')
         print name1
-        print "-------------------------------"
         print name2
+
     return render(request,'crawler/inici.html', {'SearchUser':voidSearchUser})
 
 
