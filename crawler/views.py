9327bbc3363c67f1499b60e15b2183bb6993ca39
TiberiuTasnadi <ttasnadi.olivera@gmail.com>
2017-11-18 22:55:24 +0100

    FUNCIONA!

--- crawler/views.py
+++ crawler/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
+from .models import *
 from .forms import SearchUser
 
 
@@ -9,9 +9,20 @@ def inici(request):
     if request.method == "POST":
         name1=request.POST.get('name1', '')
         name2=request.POST.get('name2', '')
-        print name1
-        print name2
-        return render(request,'crawler/information.html', {'SearchUser':voidSearchUser})
+        try:
+            user1 = User.objects.get(username=name1)
+
+        except User.DoesNotExist as e:
+            user1="No trobat"
+        try:
+            user2 = User.objects.get(username=name2)
+        except User.DoesNotExist as e:
+            user2="No trobat"
+        context = {
+            'user1':user1,
+            'user2':user2,
+        }
+        return render(request,'crawler/information.html', context)
     return render(request,'crawler/inici.html', {'SearchUser':voidSearchUser})
 def information(request):
     return render(request,'crawler/information.html')
