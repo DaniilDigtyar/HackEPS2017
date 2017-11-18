82b011b13f8548b5e8c2d142e46a5e345336f233
TiberiuTasnadi <ttasnadi.olivera@gmail.com>
2017-11-18 17:02:53 +0100

    added 2nd form

--- crawler/forms.py
+++ crawler/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
 class SearchUser(forms.Form):
-    name = forms.CharField(
-        label='Username',
+    name1 = forms.CharField(
+        label='Username1',
+        error_messages={'required': 'Introdueix un usuari'},
+    )
+    name2 = forms.CharField(
+        label='Username2',
         error_messages={'required': 'Introdueix un usuari'},
     )
--- crawler/views.py
+++ crawler/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 def inici(request):
     voidSearchUser = SearchUser()
     if request.method == "POST":
-        name=request.POST.get('name', '')
+        name1=request.POST.get('name1', '')
+        name2=request.POST.get('name2', '')
+        print name1
+        print "-------------------------------"
+        print name2
     return render(request,'crawler/inici.html', {'SearchUser':voidSearchUser})
 
 
