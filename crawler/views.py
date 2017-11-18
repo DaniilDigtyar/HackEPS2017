e30dbf3f74987ba623ca72c40fe44db7e6d0c787
Marc Capell <marc.capell@gmail.com>
2017-11-18 23:41:51 +0100

    remove print stmts

--- crawler/views.py
+++ crawler/views.py
@@ -1,6 +1,8 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import *
+from .models import User, Photos
 from .forms import SearchUser
 
 
@@ -11,19 +13,31 @@ def inici(request):
         name2=request.POST.get('name2', '')
         try:
             user1 = User.objects.get(username=name1)
-
-        except User.DoesNotExist as e:
-            user1="No trobat"
+        except User.DoesNotExist:
+            user1 = None
         try:
             user2 = User.objects.get(username=name2)
-        except User.DoesNotExist as e:
-            user2="No trobat"
+        except User.DoesNotExist:
+            user2 = None
+
         context = {
-            'user1':user1,
-            'user2':user2,
+            'user1': user1,
+            'user1_wc': get_wordcloud(user1) if user1 else [],
+            'user2': user2,
+            'user2_wc': get_wordcloud(user2) if user2 else [],
         }
         return render(request,'crawler/information.html', context)
     return render(request,'crawler/inici.html', {'SearchUser':voidSearchUser})
+
+
+def get_wordcloud(user):
+    # TODO: get from one query instead
+    wordcloud = {}
+    for photo in Photos.objects.filter(owner=user).all():
+        for tag in photo.hashtags.split(','):
+            t = tag.strip()[1:]
+            wordcloud[t] = wordcloud.setdefault(t, 0) + 1
+    return [[k, wordcloud[k]] for k in wordcloud]
+
 def information(request):
     return render(request,'crawler/information.html')
-# Create your views here.
